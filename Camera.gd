39595c2fc924e762fc97b1abc25211d03c6e818a
Rafael Belo <rafaelrpbelo@gmail.com>
2021-06-14 00:52:24 -0300

    Add support to reset zoom

--- Camera.gd
+++ Camera.gd
@@ -10,9 +10,12 @@ export(float, 20, 100) var zoom_speed = 25
 export(float, 0.5, 0.9) var zoom_speed_damp = 0.9
 export(float) var min_zoom = -8.0
 export(float) var max_zoom = 8.0
+var reseting_zoom: bool = false
+export(float, 0.01, 1) var zoom_reset_speed = 0.1
 
 # Rotation Settings
 export(float, 0.001, 0.01) var rotation_sensibility = 0.005
+var reseting_rotation = false
 
 export(NodePath) var gimbal_path
 onready var gimbal = get_node(gimbal_path)
@@ -26,8 +29,13 @@ func _process(delta):
 func _unhandled_input(event: InputEvent) -> void:
 	if event.is_action_pressed("camera_zoom_in"):
 		zoom_direction = -1
+		reseting_zoom = false
 	elif event.is_action_pressed("camera_zoom_out"):
 		zoom_direction = 1
+		reseting_zoom = false
+
+	if event.is_action_pressed("camera_reset_zoom"):
+		reseting_zoom = true
 
 func _input(event: InputEvent) -> void:
 	_bind_movement(event)
@@ -37,20 +45,32 @@ func _bind_zoom(delta: float) -> void:
 	if zoom_direction == 0:
 		pass
 
-	var new_zoom = clamp(
-		translation.z + zoom_speed * delta * zoom_direction,
-		min_zoom,
-		max_zoom
-	)
+	var new_zoom: float
 
-	translation.z = new_zoom
+	if reseting_zoom:
+		if abs(translation.z) <= 0.001:
+			reseting_zoom = false
+			zoom_direction = 0
+			pass
 
-	# Stop smoothly
-	zoom_direction *= zoom_speed_damp
+		new_zoom = lerp(translation.z, 0, zoom_reset_speed)
 
-	# Stop once the threshold was reached
-	if abs(zoom_direction) <= ZOOM_STOP_THRESHOLD:
-		zoom_direction = 0
+		translation.z = new_zoom
+	else:
+		new_zoom = clamp(
+			translation.z + zoom_speed * delta * zoom_direction,
+			min_zoom,
+			max_zoom
+		)
+
+		translation.z = new_zoom
+
+		# Stop smoothly
+		zoom_direction *= zoom_speed_damp
+
+		# Stop once the threshold was reached
+		if abs(zoom_direction) <= ZOOM_STOP_THRESHOLD:
+			zoom_direction = 0
 
 func _bind_movement(event: InputEvent) -> void:
 	if event is InputEventMouseButton and Input.is_action_just_pressed("move"):
