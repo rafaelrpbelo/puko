b8296ebc8905963e4bb591e3e5541a0478cc8cd0
Rafael Belo <rafaelrpbelo@gmail.com>
2021-06-15 21:31:25 -0300

    Add support to reset camera rotation

--- Camera.gd
+++ Camera.gd
@@ -15,7 +15,9 @@ export(float, 0.01, 1) var zoom_reset_speed = 0.1
 
 # Rotation Settings
 export(float, 0.001, 0.01) var rotation_sensibility = 0.005
-var reseting_rotation = false
+export(float, 30, 300) var reset_rotation_speed: int = 200
+export(float, 1, 5) var reset_rotation_threshold: int = 3
+var reseting_rotation: bool = false
 
 export(NodePath) var gimbal_path
 onready var gimbal = get_node(gimbal_path)
@@ -25,6 +27,7 @@ onready var player = get_node(player_path)
 
 func _process(delta):
 	_bind_zoom(delta)
+	_bind_rotation_reset(delta)
 
 func _unhandled_input(event: InputEvent) -> void:
 	if event.is_action_pressed("camera_zoom_in"):
@@ -36,6 +39,9 @@ func _unhandled_input(event: InputEvent) -> void:
 
 	if event.is_action_pressed("camera_reset_zoom"):
 		reseting_zoom = true
+
+	if event.is_action_pressed("camera_reset_rotation") and _can_reset_rotation():
+		reseting_rotation = true
 
 func _input(event: InputEvent) -> void:
 	_bind_movement(event)
@@ -86,3 +92,24 @@ func _bind_movement(event: InputEvent) -> void:
 func _bind_rotation(event: InputEvent) -> void:
 	if event is InputEventMouseMotion and Input.is_action_pressed("camera_rotate"):
 		gimbal.global_rotate(Vector3.DOWN, event.relative.x * rotation_sensibility)
+
+func _bind_rotation_reset(delta: float) -> void:
+	if not reseting_rotation:
+		return
+
+	var rotation_direction: int
+
+	if gimbal.rotation_degrees.y > 0:
+		rotation_direction = 1
+	else:
+		rotation_direction = -1
+
+	var rotation_factor = reset_rotation_speed * delta * rotation_direction
+	gimbal.rotation_degrees.y -= rotation_factor
+
+	if abs(gimbal.rotation_degrees.y) <= reset_rotation_threshold:
+		gimbal.rotation_degrees.y = 0
+		reseting_rotation = false
+
+func _can_reset_rotation() -> bool:
+	return gimbal.rotation_degrees.y != 0
